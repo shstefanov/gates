286994f2a2e5b15ce3ee9ff52611ec0807884cf4
Stefan Stefanov <stefan@localhost.localdomain>
2018-05-16 19:50:09 +0300

    Improve sending message

--- test/Gate/send.js
+++ test/Gate/send.js
@@ -19,19 +19,19 @@ describe(`Class Gate.send
       true,
       false
     ].forEach((invalid_target)=>{
-      assert.throws(()=>{ gate.send(invalid_target); },  Error );
+      assert.throws(()=>{ gate._send(invalid_target); },  Error );
     });
 
   });
 
   it("Target can't be empty error", () => {
     const gate = new Gate("gate_1");
-    assert.throws(()=>{ gate.send([]); },  Error );
+    assert.throws(()=>{ gate._send([]); },  Error );
   });
 
   it("Sends invalid target", () => {
     const gate = new Gate("gate_1");
-    assert.throws(()=>{ gate.send(["invalid_id"]); },  Error );
+    assert.throws(()=>{ gate._send(["invalid_id"]); },  Error );
   });
 
   it("Handle message", () => {
@@ -43,14 +43,15 @@ describe(`Class Gate.send
     }
 
     const gate = new ChildGate("gate_1");
-    gate.send(["gate_1"], 55);
+    gate._send(["gate_1"], 55);
 
     assert.equal(param, 55);
     assert.equal(callback, undefined);
 
   });
 
-  it("Sends message trough chain", ()=>{
+  it("Sends message trough chain (2 nodes)", ()=>{
+
     let param, callback;
     class ChildGate extends Gate {
       handle(data, cb){
@@ -62,8 +63,28 @@ describe(`Class Gate.send
     const branch = new ChildGate("branch");
     root.add(branch);
 
-    root.send(["root", "branch"], { data: 22 });
+    root.send(["branch"], { data: 22 });
     assert.deepEqual(param, { data: 22 });
+  });
+
+  it("Sends message trough chain (4 nodes)", ()=>{
+    let param, callback;
+    class ChildGate extends Gate {
+      handle(data, cb){
+        param = data, callback = cb;
+      }
+    }
+
+    const root   = new Gate("root");
+    const a1   = new Gate("a1");
+    const a2   = new Gate("a2");
+    const branch = new ChildGate("branch");
+    root.add(a1);
+    a1.add(a2);
+    a2.add(branch);
+
+    root.send(["a1", "a2", "branch"], { data: 33 });
+    assert.deepEqual(param, { data: 33 });
   });
 
 });
